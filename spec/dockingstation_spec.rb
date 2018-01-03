1f696838efa345d06ef876dce127f15ea6fc0270
alextwilson <github@alextwilson.co.uk>
2018-01-03 17:04:25 +0000

    Refactored tests to use subject

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,18 +2,15 @@ require "dockingstation"
 require "bike"
 
 describe DockingStation do
+  subject(:docking_station) {DockingStation.new}
+  subject(:bike) {docking_station.release_bike}
 
   describe '#release_bike' do
     it 'returns a bike' do
-      docking_station = DockingStation.new
-
       expect(docking_station.release_bike).to be_instance_of(Bike)
     end
 
     it 'returns a working bike' do
-      docking_station = DockingStation.new
-      bike = docking_station.release_bike
-
       expect(bike.working?).to eq true
     end
   end
