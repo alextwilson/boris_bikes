5c16a91c58ba0288841ef73b1024ab212d7d3698
alextwilson <github@alextwilson.co.uk>
2018-01-03 16:37:36 +0000

    Docking station releases working bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,21 @@
 require "dockingstation"
+require "bike"
 
 describe DockingStation do
-  it { respond_to(:release_bike) }
+
+  describe '#release_bike' do
+    it 'returns a bike' do
+      docking_station = DockingStation.new
+
+      expect(docking_station.release_bike).to be_instance_of(Bike)
+    end
+
+    it 'returns a working bike' do
+      docking_station = DockingStation.new
+      bike = docking_station.release_bike
+
+      expect(bike.working?).to eq true
+    end
+  end
+
 end
