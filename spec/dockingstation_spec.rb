f638e4b44d153bb8905fe8fb50113b264a83141d
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-04 16:40:42 +0000

    Refactoring the tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,11 +8,17 @@ describe DockingStation do
   describe '#release_bike' do
 
     it 'returns a bike' do
+      docking_station.dock(Bike.new)
       expect(bike).to be_instance_of(Bike)
     end
 
     it 'returns a working bike' do
+      docking_station.dock(Bike.new)
       expect(bike.working?).to eq true
+    end
+
+    it 'returns error when storage is empty' do
+      expect { docking_station.release_bike }.to raise_error("There are no bikes")
     end
 
   end
@@ -20,8 +26,14 @@ describe DockingStation do
   describe '#dock' do
 
     it 'stores bike' do
-      docking_station.dock(bike)
-      expect(docking_station.stored).to eq bike
+      bicycle = Bike.new
+      docking_station.dock(bicycle)
+      expect(docking_station.stored).to eq bicycle
+    end
+
+    it 'returns a storage full error when storage is not empty' do
+      docking_station.dock(Bike.new)
+      expect { docking_station.dock(Bike.new) }.to raise_error("Storage is full")
     end
 
   end
@@ -29,25 +41,8 @@ describe DockingStation do
   describe '#check_storage' do
 
     it 'checks a bike is in storage' do
-      docking_station.dock(bike)
+      docking_station.dock(Bike.new)
       expect(docking_station.check_storage).to be_instance_of(Bike)
-    end
-
-  end
-
-  describe '#no_bikes' do
-
-    it 'returns error when storage is empty' do
-      expect { docking_station.no_bikes }.to raise_error
-    end
-
-  end
-
-  describe '#storage_full' do
-
-    it 'returns a storage full error when storage is not empty' do
-      docking_station.dock(bike)
-      expect { docking_station.storage_full }.to raise_error("Storage is full")
     end
 
   end
